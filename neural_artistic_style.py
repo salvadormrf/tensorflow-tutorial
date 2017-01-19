bfaf0f892fc568870423686948a8daa8d82eee3f
Salvador Faria <salvador@bynd.com>
2017-01-19 18:23:33 -0500

    Adds initial tests

---
+++ neural_artistic_style.py
@@ -0,0 +1,16 @@
+# http://www.chioka.in/tensorflow-implementation-neural-algorithm-of-artistic-style
+
+"""
+https://github.com/jcjohnson/neural-style
+https://github.com/anishathalye/neural-style
+https://github.com/andersbll/neural_artistic_style
+https://github.com/jcjohnson/fast-neural-style
+https://github.com/alexjc/neural-doodle
+https://github.com/cysmith/neural-style-tf
+https://github.com/jcjohnson/neural-style
+https://github.com/titu1994/Neural-Style-Transfer
+https://github.com/fzliu/style-transfer
+https://github.com/woodrush/neural-art-tf
+https://github.com/log0/neural-style-painting
+"""
+
